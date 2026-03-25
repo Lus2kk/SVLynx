aa5ff12ad600fb3d04ad7dbb1c6e6ba3c7df5165
KostyaShp <kostyaandegailpr@gmail.com>
2026-03-25 22:14:17 +0300

    Move RunMigrate into internal/migration package
    - Add internal/migration/migration.go with RunMigrate helper

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,6 +4,8 @@ import (
 	"context"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/redis/go-redis/v9"
 	"github.com/svlynx/messenger/internal/auth_handler"
@@ -11,26 +13,16 @@ import (
 	"github.com/svlynx/messenger/internal/auth_service"
 	"github.com/svlynx/messenger/internal/config"
 	"github.com/svlynx/messenger/internal/email"
+	"github.com/svlynx/messenger/internal/migration"
 	"github.com/svlynx/messenger/internal/user_repository"
-	"github.com/golang-migrate/migrate/v4"
-	_ "github.com/golang-migrate/migrate/v4/database/postgres"
-    _ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
-func runMigrate(PostgresAddr string) {
-	m, err := migrate.New("file://migrations", PostgresAddr)
-	if err != nil{ 
-		panic(err)
-	}
-	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
-		panic(err)
-	}
-}
+
 
 func main(){
 	cfg := config.MustLoad()
 
-	runMigrate(cfg.PostgresAddr)
+	migration.RunMigrate(cfg.PostgresAddr)
 	
 	redisClient := redis.NewClient(&redis.Options{
 		Addr: cfg.ReddisAddr,
---
+++ internal/migration/migration.go
@@ -0,0 +1,17 @@
+package migration
+
+import (
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func RunMigrate(PostgresAddr string) {
+	m, err := migrate.New("file://migrations", PostgresAddr)
+	if err != nil{ 
+		panic(err)
+	}
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		panic(err)
+	}
+}
