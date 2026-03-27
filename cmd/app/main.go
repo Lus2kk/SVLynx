7d10519b64d2e0711c61f8e0addca77f5282d2f0
Alexander <alexander@MacBook-Air-3.local>
2026-03-27 08:20:19 -0700

    feat: start writing ws client

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"log"
-
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/redis/go-redis/v9"
--- internal/chat/ws/client.go
+++ internal/chat/ws/client.go
@@ -1,10 +1,19 @@
 package ws
 
 import (
+	
+	"log/slog"
+	"time"
 	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
+	
 )
 
+
+const (
+	MaxMessageSize = 512
+	PongWaitingTimeout = 60 
+)
 
 
 type Client struct {
@@ -22,3 +31,54 @@ func  NewClient (id uuid.UUID, connection *websocket.Conn, send chan []byte, hub
 		hub: hub,
 	}
 }
+
+
+func (client *Client) ReadPump () {
+	defer func () {
+		client.hub.unregister <- client
+		client.connection.Close()
+	} ()
+	client.connection.SetReadLimit(MaxMessageSize)
+	client.connection.SetReadDeadline(time.Now().Add(PongWaitingTimeout))
+	client.connection.SetPongHandler(func(appData string) error {
+		client.connection.SetReadDeadline(time.Now().Add(PongWaitingTimeout))
+		return nil  
+	})
+	for {
+		_, message, err := client.connection.ReadMessage()
+		if err != nil {
+			slog.Error("error of reading message ", "error", err)
+			break
+		}
+		client.hub.broadcast <- message
+	}
+	}
+
+
+	func (client *Client) WritePump() {
+		ticker := time.NewTicker(54*time.Second)
+		defer func () {
+		 ticker.Stop()
+		 client.connection.Close()
+		}()
+		for {
+			select {
+			case message, ok := <- client.send:
+			if !ok {
+				client.connection.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+				 if err := client.connection.WriteMessage(websocket.TextMessage, message); err != nil {
+					 slog.Error("Error of sending message", "error", err.Error())
+					 return
+				 }
+
+			case <-ticker.C: 
+
+
+				
+			}
+		}
+
+	}
+
